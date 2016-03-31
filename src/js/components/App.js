76a8ac5bf497df42124415a0ad7ef360b88870f0
martin byrne <marb61a@gmail.com>
2016-03-31 17:43:44 +0000

    updated appApi

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,6 +1,14 @@
 var React = require('react');
 var AppActions = require('../actions/AppActions');
 var AppStore = require('../stores/AppStore');
+var SearchForm = require('./SearchForm.js');
+var MovieResults = require('./MovieResults.js');
+
+function getAppState(){
+	return {
+		movies: AppStore.getMovieResults()
+	}
+}
 
 var App = React.createClass({
 	render: function(){
